c14bd554e6d800b0f712db51ed51c7984652d42d
Mrinalini Sinha <ms6471@nyu.edu>
2017-08-10 11:20:13 -0700

    Added logic for computing grade statistics

---
+++ GradeBook/GradeBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+        public GradeBook()
+        {
+            Grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach (float grade in Grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / Grades.Count;
+            return stats;
+        }
+
+        public void AddGrade(float grade)
+        {
+            Grades.Add(grade);
+        }
+
+        public List<float> Grades;
+
+    }
+}
---
+++ GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Grades
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(93);
+            book.AddGrade(89.5f);
+			book.AddGrade(46);
+			book.AddGrade(97);
+			book.AddGrade(84);
+
+			GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine($"Class average: {stats.AverageGrade}");
+			Console.WriteLine($"Class Highest: {stats.HighestGrade}");
+			Console.WriteLine($"Class Lowest: {stats.LowestGrade}");
+		}
+    }
+}
