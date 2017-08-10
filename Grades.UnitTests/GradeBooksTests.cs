07fc354653744d8ca6bac7685821c91c6788ea89
Mrinalini Sinha <ms6471@nyu.edu>
2017-08-10 11:41:52 -0700

    completed initial unit tests

---
+++ Grades.UnitTests/GradeBooksTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using Grades;
+
+namespace UnitTests
+{
+    [TestFixture()]
+    public class GradeBookTests
+    {
+        [Test()]
+        public void ComputesHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(99);
+            book.AddGrade(23);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Assert.AreEqual(99, stats.HighestGrade);
+        }
+    }
+}
